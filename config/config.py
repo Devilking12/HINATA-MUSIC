f89143fc00edb1370a72b301a218ab0b4518ce1f
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2025-02-07 12:19:01 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -72,7 +72,7 @@ SONG_DOWNLOAD_DURATION = int(
 
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002302134961"))
 
 
 # Your User ID.
